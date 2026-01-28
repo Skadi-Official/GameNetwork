d0f73c1e04073f05e1ec0a7583462eb8efb034c3
Skadi-Official <1309673892@qq.com>
2026-01-28 14:30:03 +0800

    尝试复现输入同步

---
+++ Server/SyncInputWithoutFixedNumber/Main.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+namespace SyncInputWithoutFixedNumber
+{
+    public class SyncInputWithoutFixedNumberApp : AppBase
+    {
+        private UDPSession m_UDPServer;
+        
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+
+        protected override bool OnRun(float curTimestamp)
+        {
+            return base.OnRun(curTimestamp);
+        }
+    }
+
+    internal static class SyncInputWithoutFixedNumber
+    {
+        public static void Main(string[] args)
+        {
+            var app = new SyncInputWithoutFixedNumberApp();
+            app.Run();
+        }
+    }
+}
+
