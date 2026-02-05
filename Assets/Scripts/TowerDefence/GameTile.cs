734f6d88f05520744137bb9310df7c24588d4dac
Skadi-Official <1309673892@qq.com>
2026-02-05 15:52:40 +0800

    style:修改GameTile中的命名

--- Assets/Scripts/TowerDefence/GameTile.cs
+++ Assets/Scripts/TowerDefence/GameTile.cs
@@ -2,15 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace TowerDefence
 {
     public class GameTile : MonoBehaviour
     {
         [SerializeField] private Transform arrow;
-        [SerializeField] private GameTile m_NorthTile, m_SouthTile, m_EastTile, m_WestTile;
-        [SerializeField] private GameTile m_NextOnPath; // 走到该网格后下一格该走哪里
-        [SerializeField] private int m_distance; // 离目的地的最短距离
+        [SerializeField] private GameTile northTile, southTile, eastTile, westTile;
+        [SerializeField] private GameTile nextOnPath; // 走到该网格后下一格该走哪里
+        [SerializeField] private int distance; // 离目的地的最短距离
         public bool IsAlternative { get; set; }
         #region 提供给箭头的旋转角度
 
@@ -23,7 +24,7 @@ namespace TowerDefence
         /// <summary>
         /// 当前网格是否可达目的地
         /// </summary>
-        public bool HasPath => m_distance != int.MaxValue;
+        public bool HasPath => distance != int.MaxValue;
         /// <summary>
         /// 绑定两个东西相邻网格之间的东西邻居关系
         /// </summary>
@@ -32,10 +33,10 @@ namespace TowerDefence
         public static void MakeEastWestNeighbor(GameTile eastTile, GameTile westTile)
         {
             // 如果assert里面的条件不成立，程序会立即中断并抛出错误信息
-            Debug.Assert(westTile.m_EastTile == null && eastTile.m_WestTile == null,
+            Debug.Assert(westTile.eastTile == null && eastTile.westTile == null,
                 $"Redefined neighbors! at {eastTile.gameObject.name} and {westTile.gameObject.name}");
-            eastTile.m_WestTile = westTile;
-            westTile.m_EastTile = eastTile;
+            eastTile.westTile = westTile;
+            westTile.eastTile = eastTile;
         }
 
         /// <summary>
@@ -45,25 +46,25 @@ namespace TowerDefence
         /// <param name="southTile">南方网格</param>
         public static void MakeNorthSouthNeighbor(GameTile northTile, GameTile southTile)
         {
-            Debug.Assert(northTile.m_SouthTile == null && southTile.m_NorthTile == null,
+            Debug.Assert(northTile.southTile == null && southTile.northTile == null,
                 $"Redefined neighbors! at {northTile.gameObject.name} and {southTile.gameObject.name}");
-            northTile.m_SouthTile = southTile;
-            southTile.m_NorthTile = northTile;
+            northTile.southTile = southTile;
+            southTile.northTile = northTile;
         }
 
         public void ShowPath()
         {
-            if (this.m_distance == 0)
+            if (this.distance == 0)
             {
                 arrow.gameObject.SetActive(false);
                 return;
             }
             arrow.gameObject.SetActive(true);
-            arrow.localRotation = m_NextOnPath switch
+            arrow.localRotation = nextOnPath switch
             {
-                var n when n == m_NorthTile => NorthRotation,
-                var e when e == m_EastTile  => EastRotation,
-                var s when s == m_SouthTile => SouthRotation,
+                var n when n == northTile => NorthRotation,
+                var e when e == eastTile  => EastRotation,
+                var s when s == southTile => SouthRotation,
                 _                                   => WestRotation // _ 代表默认情况
             };
         }
@@ -75,8 +76,8 @@ namespace TowerDefence
         /// </summary>
         public void ClearPath()
         {
-            m_distance = int.MaxValue;
-            m_NextOnPath = null;
+            distance = int.MaxValue;
+            nextOnPath = null;
         }
 
         /// <summary>
@@ -84,8 +85,8 @@ namespace TowerDefence
         /// </summary>
         public void BecomeDestination()
         {
-            m_distance = 0;
-            m_NextOnPath = null;
+            distance = 0;
+            nextOnPath = null;
         }
 
         /// <summary>
@@ -100,15 +101,15 @@ namespace TowerDefence
             {
                 return null;
             }
-            neighbor.m_distance = m_distance + 1;
-            neighbor.m_NextOnPath = this;
+            neighbor.distance = distance + 1;
+            neighbor.nextOnPath = this;
             return neighbor;
         }
         
-        public GameTile GrowPathNorth() => GrowPathTo(m_NorthTile);
-        public GameTile GrowPathSouth() => GrowPathTo(m_SouthTile);
-        public GameTile GrowPathEast() => GrowPathTo(m_EastTile);
-        public GameTile GrowPathWest() => GrowPathTo(m_WestTile);
+        public GameTile GrowPathNorth() => GrowPathTo(northTile);
+        public GameTile GrowPathSouth() => GrowPathTo(southTile);
+        public GameTile GrowPathEast() => GrowPathTo(eastTile);
+        public GameTile GrowPathWest() => GrowPathTo(westTile);
         
         #endregion
         
